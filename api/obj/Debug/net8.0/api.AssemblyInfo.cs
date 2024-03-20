29e81f113d969f0654d10c37a5e95e375d6b711b
kevin kim <sycodes95@gmail.com>
2024-03-19 17:54:02 -0700

    fixed bug where portfolio endpoint was sending 500 error

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38bd3ac709b25f0e45180d181ae54516657d4cab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804160bd90f5eb3319dd9bf9cf3104b4d2237e0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
